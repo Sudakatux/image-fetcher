c133959d398cff450122328982a5c25b5f54b99b
James Stuart Milne <jstuartmilne@gmail.com>
2022-01-11 16:32:03 -0300

    feat: fetches for images in infinite scroll

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState, useCallback } from "react";
+
+const baseUrl = "http://www.pinkvilla.com/";
+const pageSuffix = "/photo-gallery-feed-page/page";
+const buildPageUrl = (page) =>
+  page === 0 ? `${baseUrl}${pageSuffix}` : `${baseUrl}${pageSuffix}/${page}`;
+const fetchImages = async (page) => (await fetch(buildPageUrl(page))).json();
 
 function App() {
+  const [currentPage, setPage] = useState(0);
+  const [images, setImages] = useState([]);
+  // const [isFetching, setIsFetching] = useState(false);
+  const incrementPage = useCallback(() => {
+    setPage(currentPage=>currentPage + 1);
+  }, [setPage]);
+
+  const addImagesToBuffer = useCallback(
+    (newImages) => {
+      setImages(images.concat(newImages));
+    },
+    [images, setImages]
+  );
+
+  const trackScrolling = useCallback(() => {
+    const appElement = document.getElementById("app");
+    if (appElement.getBoundingClientRect().bottom <= window.innerHeight) {
+      console.log("Gettign called");
+      incrementPage();
+    }
+  }, [incrementPage]);
+
+  useEffect(() => {
+    fetchImages(currentPage).then(({ nodes }) => {
+      addImagesToBuffer(nodes);
+    });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentPage]);
+
+  useEffect(() => {
+    document.addEventListener("scroll", trackScrolling);
+  }, [trackScrolling]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="App" id="app" style={{ height: "100%" }}>
+      <div>
+        <ul
+        // ref={containerRef}
+        // onScroll={handleScroll}
+        // style={{ height: 1600, border: "1px solid" }}
         >
-          Learn React
-        </a>
-      </header>
+          {images.map(
+            ({ node: { title, path, field_photo_image_section, nid } }) => (
+              <li>
+                <a href={`${baseUrl}${path}`} id={nid}>
+                  {title}
+                </a>
+                <img
+                  src={`${baseUrl}${field_photo_image_section}`}
+                  alt={title}
+                />
+              </li>
+            )
+          )}
+        </ul>
+      </div>
+
+      <div></div>
     </div>
   );
 }
